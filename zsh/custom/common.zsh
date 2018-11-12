62c11bb86887e344b0ae51deaf631c01ed4e5d9b
Alexander Herrmann <darignac@gmail.com>
2018-11-12 08:44:18 +0100

    Update common.zsh

--- zsh/custom/common.zsh
+++ zsh/custom/common.zsh
@@ -1,13 +1,11 @@
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
-
 alias copy_dvd_regionfree="vobcopy -v -m -F 16 /media/cdrom1"
 alias remove_brackets="rename -v 's/\((.*)\)(.*)/$1$2/'"
 alias clam_scan="clamscan / -ir --exclude-dir=^/sys --exclude-dir=^/dev --exclude-dir=^/proc --detect-pua=yes --scan-mail=yes --log=$HOME/clamscan.log"
 alias flac_replaygain="metaflac --add-replay-gain *.flac"
 alias flac_cover_art="metaflac --import-picture-from"
-
 function remove_trailing_whitespace() {
     echo -n `cat $1` > $1
 }
