451e7d17e188899644998302ebba0f72aa1cb2c6
Alexander Herrmann <darignac@gmail.com>
2018-11-12 08:44:58 +0100

    Update docker.zsh

--- zsh/custom/docker.zsh
+++ zsh/custom/docker.zsh
@@ -4,23 +4,18 @@ alias dci="docker images --no-trunc | grep none | awk '{print \$3}' | xargs -r d
 alias di="docker images"
 alias dps="docker ps -a"
 alias dcp="docker-compose"
-
 function dci_grep() {
     docker images --no-trunc | grep $1 | awk '{print $3}' | xargs -r docker rmi
 }
-
 function dcp2() {
     dcp stop && dcc
 }
-
 function dcp3() {
     dcp up -d && dcp logs -f
 }
-
 function dcp4() {
     dcp stop && dcc && dcp up -d && dcp logs -f
 }
-
 function dcp5() {
     dcp stop && dcc && dcp build && dcp up -d && dcp logs -f
 }
