85ee5de744fff9a48aa35143af6ccc58344e4438
Alexander Herrmann <darignac@gmail.com>
2018-11-12 08:46:15 +0100

    Update vagrant.zsh

--- zsh/custom/vagrant.zsh
+++ zsh/custom/vagrant.zsh
@@ -3,12 +3,10 @@ alias vd="vagrant destroy"
 alias vr="vagrant reload"
 alias vst="vagrant status"
 alias vh="vagrant halt"
-
 function vu() {
     vagrant up $1
     vagrant ssh -c "curl -s https://raw.githubusercontent.com/darignac/fx/master/.bash_aliases >> /home/vagrant/.bash_aliases" $1
 }
-
 function vu2() {
     vagrant up $1
     vagrant ssh -c "curl -s https://raw.githubusercontent.com/darignac/fx/master/.bash_aliases >> /home/vagrant/.bash_aliases" $1
