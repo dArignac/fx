65c0defae974b04ccdcb16afe6c8f0785692754f
Alexander Herrmann <darignac@gmail.com>
2019-12-27 14:23:02 +0100

    added flutter path

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -14,5 +14,7 @@ PATH_ZSH=$PATH_ZSH:$HOME/.yarn/bin
 PATH_ZSH=$PATH_ZSH:/usr/local/go/bin
 # python
 PATH_ZSH=$PATH_ZSH:$HOME/.local/bin
+# flutter
+PATH_ZSH=$PATH_ZSH:$HOME/projects/github/flutter/bin
 # set the PATH variable
 export PATH=$PATH:$PATH_ZSH
