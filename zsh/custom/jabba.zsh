efe2e181a73b56f7c93d8f2b36af5bbe1b806050
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-06-20 13:12:02 +0200

    fixed jabba initialization

--- zsh/custom/jabba.zsh
+++ zsh/custom/jabba.zsh
@@ -1,2 +1,1 @@
-[ -s "$JABBA_HOME/jabba.sh" ] && source "$JABBA_HOME/jabba.sh"
-jabba alias default openjdk@1.16.0
+[ -s "$JABBA_HOME/jabba.sh" ] && source "$JABBA_HOME/jabba.sh" && jabba alias default openjdk@1.16.0
