54001c8f601a5758ff0f1bc53fd80e384a739f88
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2021-07-31 15:00:19 +0200

    fixed microk8s

--- zsh/custom/microk8s.zsh
+++ zsh/custom/microk8s.zsh
@@ -1,1 +1,4 @@
-alias mks_access_token="microk8s kubectl -n kube-system describe secret $(microk8s kubectl -n kube-system get secret | grep default-token | cut -d \" \" -f1)"
+function mks_access_token() {
+	token=$(microk8s kubectl -n kube-system get secret | grep default-token | cut -d " " -f1)
+	microk8s kubectl -n kube-system describe secret $token
+}
