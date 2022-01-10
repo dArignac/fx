6215d2bfc6ea4cdfc2ab4df3676d5671c26e4714
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-01-10 13:35:42 +0100

    fixed check for directories and added homebrew

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -1,42 +1,45 @@
 # github folder
-[[ -d "$HOME/projects/github" ]] && PATH_ZSH=$HOME/projects/github
+[ -d "$HOME/projects/github" ] && PATH_ZSH=$HOME/projects/github
 
 # composer
-[[ -d "$HOME/.composer/vendor/bin/" ]] && PATH_ZSH=$PATH_ZSH:$HOME/.composer/vendor/bin/
+[ -d "$HOME/.composer/vendor/bin/" ] && PATH_ZSH=$PATH_ZSH:$HOME/.composer/vendor/bin/
 
 # rvm
-[[ -d "$HOME/.rvm/bin" ]] && PATH_ZSH=$PATH_ZSH:$HOME/.rvm/bin
+[ -d "$HOME/.rvm/bin" ] && PATH_ZSH=$PATH_ZSH:$HOME/.rvm/bin
 
 # ffmpeg
-[[ -d "$HOME/ffmpeg-4.0-64bit-static" ]] && PATH_ZSH=$PATH_ZSH:$HOME/ffmpeg-4.0-64bit-static
+[ -d "$HOME/ffmpeg-4.0-64bit-static" ] && PATH_ZSH=$PATH_ZSH:$HOME/ffmpeg-4.0-64bit-static
 
 # sonarcube scanner
-[[ -d "$HOME/sonar-scanner/bin" ]] && PATH_ZSH=$PATH_ZSH:$HOME/sonar-scanner/bin
+[ -d "$HOME/sonar-scanner/bin" ] && PATH_ZSH=$PATH_ZSH:$HOME/sonar-scanner/bin
 
 # yarn
-[[ -d "$HOME/.yarn/bin" ]] && PATH_ZSH=$PATH_ZSH:$HOME/.yarn/bin
+[ -d "$HOME/.yarn/bin" ] && PATH_ZSH=$PATH_ZSH:$HOME/.yarn/bin
 
 # go
-[[ -d "/usr/local/go/bin" ]] && PATH_ZSH=$PATH_ZSH:/usr/local/go/bin
-[[ -d "$HOME/go/bin" ]] && PATH_ZSH=$PATH_ZSH:$HOME/go/bin
+[ -d "/usr/local/go/bin" ] && PATH_ZSH=$PATH_ZSH:/usr/local/go/bin
+[ -d "$HOME/go/bin" ] && PATH_ZSH=$PATH_ZSH:$HOME/go/bin
 
 # python
-[[ -d "$HOME/.local/bin" ]] && PATH_ZSH=$PATH_ZSH:$HOME/.local/bin
+[ -d "$HOME/.local/bin" ] && PATH_ZSH=$PATH_ZSH:$HOME/.local/bin
 
 # flutter
-[[ -d "$HOME/projects/github/flutter/bin" ]] && PATH_ZSH=$PATH_ZSH:$HOME/projects/github/flutter/bin
+[ -d "$HOME/projects/github/flutter/bin" ] && PATH_ZSH=$PATH_ZSH:$HOME/projects/github/flutter/bin
 
 # doctoolchain
-[[ -d "$HOME/projects/github/docToolchain/bin" ]] && PATH_ZSH=$PATH_ZSH:$HOME/projects/github/docToolchain/bin
+[ -d "$HOME/projects/github/docToolchain/bin" ] && PATH_ZSH=$PATH_ZSH:$HOME/projects/github/docToolchain/bin
 
 # maven
-[[ -d "$PATH_ZSH:$HOME/apache-maven/bin" ]] && PATH_ZSH=$PATH_ZSH:$HOME/apache-maven/bin
+[ -d "$PATH_ZSH:$HOME/apache-maven/bin" ] && PATH_ZSH=$PATH_ZSH:$HOME/apache-maven/bin
 
 # azure functions (only for Ubuntu 21.04 as there is no package)
 [ -s "$HOME/azure-functions-cli/func" ] && PATH_ZSH=$PATH_ZSH:$HOME/azure-functions-cli
 
 # cuda
-[[ -d "/usr/local/cuda/bin" ]] && PATH_ZSH=$PATH_ZSH:/usr/local/cuda/bin
+[ -d "/usr/local/cuda/bin" ] && PATH_ZSH=$PATH_ZSH:/usr/local/cuda/bin
+
+# homebrew (on Mac)
+[ -d "$HOME/homebrew" ] && PATH_ZSH=$PATH_ZSH:$HOME/homebrew/bin
 
 # set the PATH variable
 export PATH=$PATH:$PATH_ZSH
