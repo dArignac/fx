7d0e0a3ba5ab0ce6421f1f5ed2957e95ed345276
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2022-11-13 13:49:11 +0100

    increased java version

--- zsh/custom/z_last-init.zsh
+++ zsh/custom/z_last-init.zsh
@@ -2,4 +2,4 @@
 # still private files (z_private_*) can come afterwards
 
 # set the var with the java version for jabba
-export JABBA_JAVA_VERSION="openjdk@1.16.0"
+export JABBA_JAVA_VERSION="openjdk@1.17.0"
